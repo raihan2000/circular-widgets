2857add7294f1d896be206f9ec96d3f38c626512
raihan2000 <raihan1999ahamed@gmail.com>
2022-11-29 23:58:04 +0530

    version bump to 4

--- src/extension.js
+++ src/extension.js
@@ -1,31 +1,35 @@
-const {St,Clutter,GObject,Shell,GLib,Gio} = imports.gi;
+const {GLib} = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
-const Cairo = imports.cairo;
 const Me = ExtensionUtils.getCurrentExtension();
 const Clock = Me.imports.circleClock.Clock;
 const Ram = Me.imports.circleRam.Ram;
 const Cpu = Me.imports.circleCpu.Cpu;
+const Calendar = Me.imports.calendarWidgets.calendar;
 
+let timeoutId;
 var clock;
 var ram;
 var cpu;
+var calendar;
 
 function init() {
-	ExtensionUtils.initTranslations();
 }
 
 function enable() {
 	clock = new Clock()
 	cpu = new Cpu();
 	ram = new Ram();
+	calendar = new Calendar();
 	Main.layoutManager._backgroundGroup.add_child(clock);
 	Main.layoutManager._backgroundGroup.add_child(cpu);
 	Main.layoutManager._backgroundGroup.add_child(ram);
-	timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
+	Main.layoutManager._backgroundGroup.add_child(calendar);
+	timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
 		clock.update();
 		cpu.update();
 		ram.update();
+		calendar._update();
 		return GLib.SOURCE_CONTINUE;
 	});
 }
@@ -38,4 +42,9 @@ function disable() {
 	Main.layoutManager._backgroundGroup.remove_child(clock);
 	Main.layoutManager._backgroundGroup.remove_child(cpu);
 	Main.layoutManager._backgroundGroup.remove_child(ram);
+	Main.layoutManager._backgroundGroup.remove_child(calendar);
+	clock = null;
+	cpu = null;
+	ram = null;
+	calendar = null;
 }
