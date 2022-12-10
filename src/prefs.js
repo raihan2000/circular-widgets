b5612dd0e1b7cc41e0e9b1c5813097884b2f959e
raihan2000 <raihan1999ahamed@gmail.com>
2022-12-10 23:41:28 +0530

    bump version 5

--- src/prefs.js
+++ src/prefs.js
@@ -107,7 +107,6 @@ class PrefsWindow {
 		}
     
   	append_spin_button(group,	title, is_double, key, min, max, step) {
-
         let v = 0;
         if (is_double) {
             v = this._settings.get_double(key);
@@ -162,11 +161,15 @@ class PrefsWindow {
 		}
 
 		append_font_chooser(group,title,key) {
+			let Font = this._settings.get_string(key);
 			let chooser = new Gtk.FontButton({
-				valign: Gtk.Align.CENTER
+				use_size: true,
+				use_font: true,
+				valign: Gtk.Align.CENTER,
+				font: Font
 			});
 			chooser.connect('font-set', (widget) => {
-      	this._settings.set_string(key, widget.get_font().to_string());
+      	this._settings.set_string(key, widget.get_font());
       });
 			this.append_row(group,title,chooser);
 		}
@@ -235,33 +238,83 @@ class PrefsWindow {
     		this.append_switch(groupClockText, 'Disable 12 hour Clock', 'am-or-pm-clock');
     		this.append_switch(groupClockText, 'Hide Text Clock', 'text-clock');
     		this.append_color_button(groupClockText,'Text Color','clock-text-color',this._settings.get_string('clock-text-color'));
-
-//    		this.append_font_chooser(groupClock,'Text Font','clock-text-font');
+    		this.append_font_chooser(groupClockText,'Text Font','clock-text-font');
     	}
 
     	let cpuWidget = this.create_page('CPU');
     	{
     		let groupCpu = this.create_group(cpuWidget);
     		this.append_switch(groupCpu,'Hide CPU','hide-cpu-widget');
-    		this.append_scale_bar(groupCpu,'Size', 'circular-cpu-size',{min: 80,max: 250,step: 1,mark_position: 100,add_mark: true,size: 200,draw_value: true});
-    		this.append_spin_button(groupCpu,'Line Width',false,'cpu-line-width',1,125,1);
-    		this.append_switch(groupCpu,'Show Inner Circle','cpu-inner-circle');
-    		this.append_color_button(groupCpu,'CPU Ring','cpu-line-color',this._settings.get_string('cpu-line-color'));
-//    		this.append_font_chooser(groupCpu,'Text Font','cpu-text-font');
-    		this.append_color_button(groupCpu,'Text Color','cpu-text-color',this._settings.get_string('cpu-text-color'));
+    		this.append_scale_bar(groupCpu,'Size', 'circular-cpu-size',{min: 80,max: 150,step: 1,mark_position: 100,add_mark: true,size: 200,draw_value: true});
+
+    		let groupCpuRing = this.create_group(cpuWidget, 'CPU Ring');
+				this.append_spin_button(groupCpuRing,'Set Start Point',true,'cpu-ring-startpoint',0,2,0.01);
+				this.append_spin_button(groupCpuRing,'Set End Point',true,'cpu-ring-endpoint',0,2,0.01);
+    		this.append_spin_button(groupCpuRing,'Ring Width',true,'cpu-line-width',1,125,0.1);
+    		this.append_spin_button(groupCpuRing,'Ring Radius',true,'cpu-ring-radius',1,74.5,0.1);
+    		this.append_switch(groupCpuRing,'Show Inner Circle','cpu-inner-circle');
+				this.append_spin_button(groupCpuRing,'Inner Circle Radius',true,'cpu-inner-circle-radius',1,74.5,0.1);
+    		this.append_color_button(groupCpuRing,'Ring Color','cpu-line-color',this._settings.get_string('cpu-line-color'));
+
+    		let groupCpuText = this.create_group(cpuWidget, 'CPU Text');
+    		this.append_font_chooser(groupCpuText,'Text Font','cpu-text-font');
+    		this.append_spin_button(groupCpuText,'Text Position X',false,'cpu-text-position-x',-75,75,1);
+    		this.append_spin_button(groupCpuText,'Text Position X',false,'cpu-text-position-y',-75,75,1);
+    		this.append_switch(groupCpuText,'Enable In-Line Text','enable-inline-cpu');
+    		this.append_spin_button(groupCpuText,'InLine Text Position X',false,'cpu-inline-text-position-x',-75,75,1);
+    		this.append_spin_button(groupCpuText,'InLine Text Position Y',false,'cpu-inline-text-position-y',-75,75,1);
+    		this.append_color_button(groupCpuText,'Text Color','cpu-text-color',this._settings.get_string('cpu-text-color'));
+
     	}
 
     	let ramWidget = this.create_page('RAM');
     	{
     		let groupRam = this.create_group(ramWidget);
     		this.append_switch(groupRam,'Hide RAM','hide-ram-widget');
-    		this.append_scale_bar(groupRam,'Size', 'circular-ram-size',{min: 80,max: 250,step: 1,mark_position: 100,add_mark: true,size: 200,draw_value: true});
-    		this.append_spin_button(groupRam,'Line Width',false,'ram-line-width',1,125,1);
-    		this.append_switch(groupRam,'Show Inner Circle','ram-inner-circle');
-    		this.append_color_button(groupRam,'RAM Ring','ram-line-color',this._settings.get_string('ram-line-color'));
-//    		this.append_font_chooser(groupRam,'Text Font','ram-text-font');
-    		this.append_color_button(groupRam,'Text Color','ram-text-color',this._settings.get_string('ram-text-color'));
+    		this.append_scale_bar(groupRam,'Size', 'circular-ram-size',{min: 80,max: 250,step: 1,mark_position: 100,add_mark: true,size: 150,draw_value: true});
+
+				let groupRamRing = this.create_group(ramWidget,'RAM Ring');
+				this.append_spin_button(groupRamRing,'Set Start Point',true,'ram-ring-startpoint',0,2,0.01);
+				this.append_spin_button(groupRamRing,'Set End Point',true,'ram-ring-endpoint',0,2,0.01);
+    		this.append_spin_button(groupRamRing,'Ring Width',false,'ram-line-width',1,74.5,0.1);
+    		this.append_spin_button(groupRamRing,'Ring Radius',true,'ram-ring-radius',1,74.5,0.1);
+    		this.append_switch(groupRamRing,'Show Inner Circle','ram-inner-circle');
+				this.append_spin_button(groupRamRing,'Inner Circle Radius',true,'ram-inner-circle-radius',1,74.5,0.1);
+    		this.append_color_button(groupRamRing,'Ring Color','ram-line-color',this._settings.get_string('ram-line-color'));
+				
+				let groupRamText = this.create_group(ramWidget,'RAM Text');
+    		this.append_font_chooser(groupRamText,'Text Font','ram-text-font');
+    		this.append_spin_button(groupRamText,'Text Position X',false,'ram-text-position-x',-75,75,1);
+    		this.append_spin_button(groupRamText,'Text Position X',false,'ram-text-position-y',-75,75,1);
+    		this.append_switch(groupRamText,'Enable In-Line Text','enable-inline-ram');
+    		this.append_spin_button(groupRamText,'InLine Text Position X',false,'ram-inline-text-position-x',-75,75,1);
+    		this.append_spin_button(groupRamText,'InLine Text Position Y',false,'ram-inline-text-position-y',-75,75,1);
+    		this.append_color_button(groupRamText,'Text Color','ram-text-color',this._settings.get_string('ram-text-color'));
+
     	}
+
+			let netSpeedWidget = this.create_page('Net Speed');
+			{
+				let groupNetSpeed = this.create_group(netSpeedWidget);
+    		this.append_switch(groupNetSpeed,'Hide Net Speed','hide-netspeed-widget');
+    		this.append_scale_bar(groupNetSpeed,'Size', 'circular-netspeed-size',{min: 80,max: 250,step: 1,mark_position: 100,add_mark: true,size: 150,draw_value: true});
+
+    		let groupNetSpeedRing = this.create_group(netSpeedWidget,'Net Speed Ring');
+				this.append_spin_button(groupNetSpeedRing,'Set Start Point',true,'netspeed-ring-startpoint',0,2,0.01);
+				this.append_spin_button(groupNetSpeedRing,'Set End Point',true,'netspeed-ring-endpoint',0,2,0.01);
+    		this.append_spin_button(groupNetSpeedRing,'Down Ring Width',false,'netspeed-down-ring-width',1,74.5,0.1);
+    		this.append_spin_button(groupNetSpeedRing,'Down Ring Radius',true,'netspeed-down-ring-radius',1,74.5,0.1);
+    		this.append_color_button(groupNetSpeedRing,'Down Ring Color','netspeed-down-ring-color',this._settings.get_string('netspeed-down-ring-color'));
+    		this.append_spin_button(groupNetSpeedRing,'Upload Ring Width',false,'netspeed-up-ring-width',1,74.5,0.1);
+    		this.append_spin_button(groupNetSpeedRing,'Upload Ring Radius',true,'netspeed-up-ring-radius',1,74.5,0.1);
+    		this.append_color_button(groupNetSpeedRing,'Upload Ring Color','netspeed-up-ring-color',this._settings.get_string('netspeed-up-ring-color'));
+    		
+    		let groupNetSpeedText = this.create_group(netSpeedWidget,'Net Speed Text');
+    		this.append_font_chooser(groupNetSpeedText,'Text Font','netspeed-text-font');
+    		this.append_spin_button(groupNetSpeedText,'Text Position X',false,'netspeed-text-position-x',-75,75,1);
+    		this.append_spin_button(groupNetSpeedText,'Text Position X',false,'netspeed-text-position-y',-75,75,1);
+    		this.append_color_button(groupNetSpeedText,'Text Color','netspeed-text-color',this._settings.get_string('netspeed-text-color'));
+			}
 
 			let calendarWidget = this.create_page('Calendar');
 			{
